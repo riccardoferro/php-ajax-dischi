2044e4ab7a7c17575325eb818d63a324c5b17177
riccardoferro <riccardo.ferron8@gmail.com>
2022-05-05 23:45:59 +0200

    add call api by axios

--- js/script.js
+++ js/script.js
@@ -8,11 +8,22 @@ const app = new Vue(
     el: "#app",
   
     data() {
-      
-            return {
 
+            return {
+              albums: [],
             }
     },
+    mounted(){
+
+      axios.get("http://localhost/PHP_Projects_Boolean/php-ajax-dischi/data/apiDischi.php").then((response)=>{
+        // console.log(response);
+        if(response.status === 200){
+          this.albums = response.data;
+          // console.log(this.albums);
+        }
+      }).catch(error => console.log(error));
+
+    }
     
   
   });
