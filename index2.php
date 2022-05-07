3c3642d1e5488ae6cc7b6f2fa9cc7fe1d435c8f2
riccardoferro <riccardo.ferron8@gmail.com>
2022-05-07 12:58:18 +0200

    print out all albums od the data

--- index2.php
+++ index2.php
@@ -1,29 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php-ajax-dischi</title>
 
-include("data/apiDischi.php");
-
-// echo($dischi);
-
-foreach ($dischi as $album) {
-  echo("<p>");
-  foreach ($album as $key => $value) {
-
-    if ($key == "poster") {
-
-       echo("<br>");
-       echo("<img src='$value'>");
-       echo("<br>");
-    }
-    else {
-      echo("<br>");
-      echo($value);
-      echo("<br>");
-
-    }
-  }
-  echo("</p>");
-
-}
+  <!-- BOOTSTRAP -->
+  <link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+  crossorigin="anonymous"
+  />
 
 
-?>
+  <!-- FONT AWESOME -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+
+  <!-- LINK CSS -->
+  <link rel="stylesheet" href="style/style.css" />
+
+</head>
+
+<body>
+
+
+<!--start  header -->
+<header>
+    <img src="img/spotify-logo.png" alt="logo spotify">
+</header>
+
+
+<div class="container pb-1 pt-1 d-flex justify-content-center">
+    <div class="row gap-2 w-75 m-auto">
+
+          <?php
+          include("data/dataDischi.php");
+          // echo($dischi);
+
+          foreach ($dischi as $album) {
+              echo("<div class='card-rf me-1 col-12 col-md-3 col-xl-2'>");
+          ?>
+          <img src="<?= $album['poster'] ?>" alt="<?= $album['title']?>" class="img-fluid pb-4">
+          <h6> <?= $album['title']?> </h6>
+          <span> <?= $album['author']?> </span>
+          <span class="d-block"> <?= $album['year']?> </span>
+
+          <?php
+              echo("</div>");
+          }
+          ?>
+    </div>
+</div>
+
+</body>
+
+</html>
